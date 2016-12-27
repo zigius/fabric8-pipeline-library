f3edabad9c680ce36216d1420a979c5f202262c2
Daniel Zinger <daniel.zinger@DanielZ-mac.local>
2016-12-27 18:49:52 +0200

    split jenkins

--- vars/mergeToStaging.groovy
+++ vars/mergeToStaging.groovy
@@ -1,24 +1,24 @@
 #!groovy?
 def call(body) {
 
-  echo "in github merge"
   // evaluate the body block, and collect configuration into the object
   def config = [:]
   body.resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = config
   body()
 
-  echo config.branchName 
-  echo body
-  echo "passed body parsing"
+  env.BRANCH_NAME=config.branchName
 
-        // git config user.name 'ci.infra'
-        // git config user.email 'ci.infra@ironsource.com'
-        // git checkout staging
-        // git merge ${config.branchName}
-        // echo env.GIT_COMMIT=$(git rev-parse HEAD) > merge.properties
-        // echo env.GIT_BRANCH=staging >> merge.properties
-        // sed 's/$/"/g' -i merge.properties
-        // sed 's/=/="/g' -i merge.properties
-    // load ('merge.properties')
+  sh '''
+    git config user.name 'ci.infra'
+    git config user.email 'ci.infra@ironsource.com'
+    git checkout staging
+    git merge ${BRANCH_NAME}
+    echo env.GIT_COMMIT=$(git rev-parse HEAD) > merge.properties
+    echo env.BRANCH_NAME=staging >> merge.properties
+    sed 's/$/"/g' -i merge.properties
+    sed 's/=/="/g' -i merge.properties
+  '''
+
+  load ('merge.properties')
 }
