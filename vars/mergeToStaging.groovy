1eabeca9f41dd57ffa3121cf856e0a5bbb0dba88
Daniel Zinger <daniel.zinger@DanielZ-mac.local>
2016-12-27 18:01:56 +0200

    split jenkins

---
+++ vars/mergeToStaging.groovy
@@ -0,0 +1,21 @@
+#!groovy?
+def call(body) {
+
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    sh '''
+        git config user.name 'ci.infra'
+        git config user.email 'ci.infra@ironsource.com'
+        git checkout staging
+        git merge ${config.BRANCH_NAME}
+        echo env.GIT_COMMIT=$(git rev-parse HEAD) > merge.properties
+        echo env.GIT_BRANCH=staging >> merge.properties
+        sed 's/$/"/g' -i merge.properties
+        sed 's/=/="/g' -i merge.properties
+    '''
+    load ('merge.properties')
+}
